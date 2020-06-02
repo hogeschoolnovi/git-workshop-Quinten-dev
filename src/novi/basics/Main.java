c847e377344ad2ae7c840c8f9aa63b599f8f6248
Quinten Schreutelkamp <q.schreutelkamp@novi-education.nl>
2020-06-02 19:19:43 +0200

    logincontroller

--- src/novi/basics/Main.java
+++ src/novi/basics/Main.java
@@ -1,9 +1,12 @@
 package novi.basics;
 
-public class Main {
+public class  Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello World");
+        System.out.println("Hello Quinten");
+        LoginController loginController = new LoginController();
+
+
     }
 }
